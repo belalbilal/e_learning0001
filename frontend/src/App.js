af170406afaf17fbbbc80492efa3821525d07e58
belalbilal <bilal.h2r96@gmail.com>
2021-02-28 03:31:00 +0000

    file upload
    first update

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Container>
+          <h1 className='text-center'>Welcome to e learning platform</h1>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default App;
